c4ee2969c02500c2f5c61f9bf1edf729b67da34a
bobd50 <galavu10@gmail.com>
2023-02-20 09:08:28 +0300

    First commit

--- src/components/cards/ShowPaymentInfo.js
+++ src/components/cards/ShowPaymentInfo.js
@@ -1,46 +1,47 @@
 import React from 'react';
 import './showShipmentInfo.css'
+import dayjs from "dayjs";
 
 const ShowPaymentInfo = ({order, showStatus = true}) => {
     console.log(order)
 
     return (
-
         <>
+
 
             <div className="container withRibbon mt-3 mt-md-5">
                 <div className="card" data-label={order.orderStatus}>
                     <div className="card__container">
                         <div className="card-header">
-                            <h3 className='card-title p-0 m-0'>Order {order.orderId}</h3>
+                            <h1 className='card-title p-0 m-0'>Order {order.orderId}</h1>
                         </div>
                         <div className="card-body">
                             <div className="py-3">
-                                <div className="d-flex align-items-center justify-content-between">
-
-                                    <div>
+                                <div className="items">
+                                    <div className=''>
                                         <h6><strong>Payment Status</strong></h6>
                                         <p className="text-body mb-0">{order.paymentStatus.toUpperCase()}</p>
                                     </div>
-                                    <div>
+                                    <div className=''>
                                         <h6><strong>Currency</strong></h6>
                                         <p className="text-body mb-0">{order.currencyCode}</p>
                                     </div>
-                                    <div>
+                                    <div className='flex-column'>
                                         <h6><strong>Payment</strong></h6>
                                         <p className="text-body mb-0">{order.paymentMethod.toUpperCase()}</p>
                                     </div>
-                                    <div>
+                                    <div className='flex-column'>
                                         <h6><strong> Date</strong></h6>
-                                        <p className="text-body mb-0">{order.orderDate}</p>
+                                        <p className="text-body mb-0">{}
+                                            {dayjs(order.orderDate).format('MMMM D, YYYY')}
+                                        </p>
                                     </div>
-
-                                    <div>
+                                    <div className='flex-column'>
                                         <h6><strong>Amount </strong></h6>
                                         <span> {(order.amount).toLocaleString('en-US', {
                                             style: 'currency', currency: `${order.currencyCode}`
                                         })}
-                                       </span>
+                                           </span>
                                     </div>
                                     <div>
                                         <a href="/" className="btn btn-primary w-100">View Order</a>
@@ -49,16 +50,10 @@ const ShowPaymentInfo = ({order, showStatus = true}) => {
                             </div>
                         </div>
                     </div>
-
-
                 </div>
-
-
             </div>
 
         </>
-
-
     );
 };
 
